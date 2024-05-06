c3b9e1d447a8bc8b2d59f0903a3a1337186cc51b
Abdul Rehman <a.rehman.nazar@gmail.com>
2024-05-06 16:48:05 +0500

    UI is completed with compose

--- app/src/main/java/com/ar/backgroundlocation/MainActivity.kt
+++ app/src/main/java/com/ar/backgroundlocation/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.ar.backgroundlocation
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.ar.backgroundlocation.ui.theme.BackGroundLocationTheme
 
@@ -19,28 +25,38 @@ class MainActivity : ComponentActivity() {
             BackGroundLocationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun App() {
+    val context = LocalContext.current
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BackGroundLocationTheme {
-        Greeting("Android")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = {
+            //Start Service
+            Toast.makeText(context, "Service Start button clicked", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Start Service")
+        }
+        Button(onClick = {
+            //Stop Service
+            Toast.makeText(context, "Service Stop button clicked", Toast.LENGTH_SHORT).show()
+        }) {
+            Text(text = "Stop Service")
+        }
     }
 }
+
+
